e77c616bb8e8c6d992a6732681afde3be0dd0c78
ahamedsahib <adsahib39@gmail.com>
2021-07-01 12:07:55 +0530

    [Ahamed]Find Even or Odd

---
+++ BasicCorePrograms/EvenOdd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    class EvenOdd
+    {
+        public static void ReadInput()
+        {
+            Console.WriteLine("Enter Number to find Even or Odd ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            FindEvenOdd(num);
+        }
+        private static void FindEvenOdd(int num)
+        {
+            if (num % 2 == 0)//if number divide by 2 it is even else odd
+            {
+                Console.WriteLine($"The Number {num} is EVEN");
+            }
+            else
+            {
+                Console.WriteLine($"The Number {num} is ODD");
+            }
+        }
+    }
+}
