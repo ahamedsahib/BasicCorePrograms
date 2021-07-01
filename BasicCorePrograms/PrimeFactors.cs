bb10b385dde2be3b5ee1d45d55ae77cb6f264d76
ahamedsahib <adsahib39@gmail.com>
2021-07-01 11:32:33 +0530

    [Ahamed]Finding prime factors of N

---
+++ BasicCorePrograms/PrimeFactors.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    class PrimeFactors
+    {
+        public static void ReadInput()
+        {
+            Console.WriteLine("Enter The Number to Find Prime factors ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Prime Factors of {num}");
+            FindPrimeFactors(num);
+        }
+        private static void FindPrimeFactors(int num)
+        {
+            int isPrime = 1;//to check whether the number is prime or not 
+            for (int i = 2; i<= num; i++)
+            {
+                
+                if (num % i == 0)
+                {
+                    isPrime = 1;
+                    for (int j = 2; j <= i / 2; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            isPrime = 0;
+                            break;
+                        }
+                    }
+   
+                    if (isPrime == 1)
+                    {
+                        Console.WriteLine(i); 
+                    }
+                }
+            }
+        }
+    }
+}
