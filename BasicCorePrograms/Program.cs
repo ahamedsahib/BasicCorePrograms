31861a764b0e27f79883edf88bb958a39f6e2d7f
ahamedsahib <adsahib39@gmail.com>
2021-07-01 11:59:26 +0530

    [Ahamed]Swap Two Numbers

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -8,7 +8,8 @@ namespace BasicCorePrograms
         {
             Console.WriteLine("Welcome To Basic Core Programs");
             Console.WriteLine("Choose The Program You Want Run");
-            Console.WriteLine("1)Flip Coin\n2)Leap Yaar\n3)PowerOf2\n4)Harmonic Number\n5)Prime Factor\n6)Qutient&Remainder\n7)Exit");
+            Console.WriteLine("1)Flip Coin\n2)Leap Yaar\n3)PowerOf2\n4)Harmonic Number\n5)Prime Factor\n6)Qutient&Remainder");
+            Console.WriteLine( "7)SwapNumbers\n8)Exit");
             Console.WriteLine("Choose one");
             int option = Convert.ToInt32( Console.ReadLine());
             switch (option)
@@ -30,6 +31,9 @@ namespace BasicCorePrograms
                     break;
                 case 6:
                     QuotientRemainder.ReadInput();
+                    break;
+                case 7:
+                    SwapNumbers.ReadInput();
                     break;
                 default:
                     Console.WriteLine("Choose Correct Option");
