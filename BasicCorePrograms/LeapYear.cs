d21efb6413b4fa87cbe698e1f563bcf29c9b1072
ahamedsahib <adsahib39@gmail.com>
2021-07-01 07:44:30 +0530

    [Ahamed]Find Leap Year or Not

---
+++ BasicCorePrograms/LeapYear.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    class LeapYear
+    {
+        public static void ReadInput()
+        {
+            Console.Write("Input an year : ");
+            int year = Convert.ToInt32(Console.ReadLine());//read Year from User
+            FindLeapYear(year);
+        }
+
+        private static void FindLeapYear(int year)
+        {
+            if ((year % 400) == 0)
+            {
+                Console.WriteLine("{0} is a leap year.\n", year);
+            }
+            else if ((year % 100) == 0)
+            {
+                Console.WriteLine("{0} is not a leap year.\n", year);
+            }
+            else if ((year % 4) == 0)
+            {
+                Console.WriteLine("{0} is a leap year.\n", year);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a leap year.\n", year);
+            }
+        }
+    }
+}
+
