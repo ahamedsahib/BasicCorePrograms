b565a068b9e5f28913a6a26c26621fac098b7627
ahamedsahib <adsahib39@gmail.com>
2021-07-01 06:46:02 +0530

    [Ahamed]Welcome to Basic Core Program

---
+++ BasicCorePrograms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicCorePrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Basic Core Programs");
+        }
+    }
+}
